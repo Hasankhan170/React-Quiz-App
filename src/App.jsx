29ea3c5cd35d538a6974622bd50916c91ca745c8
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-04 08:01:39 -0700

    some issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 function App(){
   const [render,setRender] = useState([])
   const [questionState,setQuestionState] = useState(0)
+  const [selectAnswer,setSelectAnswer] = useState('')
 
   try {
     useEffect(()=>{
@@ -23,12 +24,24 @@ function App(){
     
   }
 
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    
+    return array;
+  }
+
   function NextQuetsion(){
     setQuestionState(questionState + 1)
     if(questionState === render.length - 1){
       setQuestionState(0)
       alert('Complete Quiz')
     }
+
+    setSelectAnswer("");
   }
   return (
     <>
@@ -37,9 +50,11 @@ function App(){
       render.length > 0 ? <div>
         <h1>Q{questionState + 1} : {render[questionState].question.text}</h1>
         <ul>
-          {[...render[questionState].incorrectAnswers , render[questionState].correctAnswer].map((item,index)=>{
+          {shuffleArray([...render[questionState].incorrectAnswers , render[questionState].correctAnswer]).map((item,index)=>{
             return <div key={index}>
-              <li>{item}</li>
+              <li><input id={`${index}`} name="quiz" type="radio" value={item} checked ={selectAnswer === item} onChange={(e)=>setSelectAnswer(e.target.value)} />
+              <label htmlFor={`${index}`}>{item}</label>
+              </li>
             </div>
           })}
         </ul>

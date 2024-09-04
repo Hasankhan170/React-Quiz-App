5a847c494dace25b598abc145037798a1e178629
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-04 03:36:09 -0700

    again axios done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import axios from "axios"
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 function App(){
+
+  const [render,setRender] =useState([])
+
+  try {
+    useEffect(()=>{
+      axios('https://the-trivia-api.com/v2/questions')
+    .then((res)=>{
+      console.log(res.data)
+    })
+    .catch((err)=>{
+      console.log(err);  
+    })
+    },[])
+  } catch (error) {
+    console.log(error);
+    
+  }
   return (
     <h1>hello</h1>
   )

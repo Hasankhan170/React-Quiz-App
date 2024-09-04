8ee77a8679efa89bcf2a96ef2927f846a1b04e5a
Hasankhan170 <hasankhanali0316@gmail.com>
2024-09-04 03:24:08 -0700

    again work

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,56 @@
 import axios from "axios"
+import { useState } from "react"
 import { useEffect } from "react"
 
 function App(){
 
+  const [render,setRender] = useState([])
+  const [questionState ,setQuestionState] = useState(0)
+
+try {
   useEffect(()=>{
     axios('https://the-trivia-api.com/v2/questions')
     .then((res)=>{
       console.log(res.data);
+      setRender(res.data)
     })
     .catch((err)=>{
       console.log(err);
     })
   },[])
+} catch (error) {
+  console.log(error);
+  
+}
+
+function nextQuestion(){
+  setQuestionState(questionState + 1)
+  if(questionState >= render.length -1){
+    alert('complete')
+    setQuestionState(0)
+  }
+}
 
 
   return (
-    <h1>Hello world</h1>
+    <>
+    <h1>Quiz App</h1>
+    {render.length > 0 ? <div>
+      <h1>Q{questionState + 1}:{render[questionState].question.text}</h1>
+      <ul>
+        {
+        ([...render[questionState].incorrectAnswers , render[questionState].correctAnswer]).map((item,index)=>{
+            return <div key={index}>
+              <li>{item}</li>
+            </div>
+          })
+        }
+      </ul>
+      <button onClick={nextQuestion}>next</button>
+
+    </div>:<h1>Loading...</h1>}
+
+    </>
   )
   
 }
